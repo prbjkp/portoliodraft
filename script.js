4e89d199777928a85183db873399a50fd9e99733
prbjkp <peterbkopp@gmail.com>
2025-12-14 04:56:58 +0000

    perahsp ai loked in

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ function animateSphere(){
   if(!isDragging) targetRotY+=autoRotateSpeed;
   rotX += (targetRotX - rotX)*0.1;
   rotY += (targetRotY - rotY)*0.1;
-  sphere.style.transform = `rotateX(${rotX}deg) rotateY(${rotY}deg) translateZ(1200px)`;
+  sphere.style.transform = `rotateX(${-rotX}deg) rotateY(${-rotY}deg) translateZ(1200px)`;
   photos.forEach((photo,i)=>{
     const pos=positions[i];
     const dx=-pos.x, dy=-pos.y, dz=-pos.z;
