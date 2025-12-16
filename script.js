106ede2bab774ada0970c4680fe7276fc6da4ef3
prbjkp <peterbkopp@gmail.com>
2025-12-16 07:35:56 +0000

    yehaw ts

--- script.js
+++ script.js
@@ -233,6 +233,6 @@ animateSphere();
 
 // Final console logs
 console.log("Script is running!");
-console.log("Final fix: 0 degrees rotation applied");
+console.log("16x9 aspect ratio");
 console.log("Number of photos:", photos.length);
 console.log("Sphere radius:", sphereRadius);
