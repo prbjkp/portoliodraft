1ef56601eb4d9e06afcaece13a73a4953ff95628
prbjkp <peterbkopp@gmail.com>
2025-12-16 17:45:07 +0000

    expand sphere

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ const autoRotateSpeed = 0.02;
 /**********************************************************
  * SPHERE LAYOUT (PHOTO DISTRIBUTION)
  **********************************************************/
-const sphereRadius = 2000;
+const sphereRadius = 2200;
 const positions = [];
 
 function computePositions() {
